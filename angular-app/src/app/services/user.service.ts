42c9583d514994b5db9f104dc3e028afe66716f8
Nidhalnsiri <141747583+Nidhalnsiri@users.noreply.github.com>
2026-03-06 09:16:59 +0000

    pip

--- angular-app/src/app/services/user.service.ts
+++ angular-app/src/app/services/user.service.ts
@@ -8,26 +8,23 @@ import { User } from '../models/User';
 })
 export class UserService {
 
-  baseURL:string="http://192.168.187.9:8081/user"
+  baseURL:string="http://localhost:8081/user"
   constructor(private  httpClient:HttpClient) { }
 
   getAllUsers():Observable<any>{
-    let headers = new HttpHeaders();
-    headers.append("Access-Control-Allow-Origin", "*")
+   
     return this.httpClient.get(this.baseURL + "/all",{headers})
   }
 
   deleteUser(id:string):Observable<any>{
-    let headers = new HttpHeaders();
-    headers.append("Access-Control-Allow-Origin", "*")
+    
     return this.httpClient.delete(this.baseURL + "/delete/"+id,{headers})
   }
 
   addUser(firstname:string,lastname:string):Observable<any>{
     let body = { firstName: firstname, lastName: lastname };
     let url=this.baseURL + "/add";
-    let headers = new HttpHeaders();
-    headers.append("Access-Control-Allow-Origin", "*")
+    
     return this.httpClient.post(url,body,{headers})
   }
 }
